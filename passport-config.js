8f0ce6193ead5361a09cba9db85a1564dc65601a
dwinatech <diwina1993@hotmail.com>
2020-01-27 21:27:03 +0000

    Remove the console.log

--- passport-config.js
+++ passport-config.js
@@ -2,25 +2,23 @@ const bcrypt = require("bcrypt");
 const LocalStrategy = require("passport-local").Strategy;
 
 const findUserByEmail = (email, users) => {
-    const user = users.find(user => user.email === email);
-    return user;
-  };
+  const user = users.find(user => user.email === email);
+  return user;
+};
 
-  const findUserById = (id, users) => {
-    const user = users.find(user => user.id === id);
-    console.log('user user user ====', user);
-    
-    return user;
-  };
+const findUserById = (id, users) => {
+  const user = users.find(user => user.id === id);
+  return user;
+};
 
 const passportConfig = (passport, users) => {
-    const callback = async (email, password, done) => {
+  const callback = async (email, password, done) => {
     try {
       const user = await findUserByEmail(email, users);
       if (!user) {
         return done(null, false, { message: "Incorrect username." });
       }
-    //   const validPassword = await bcrypt.compare(password, user.password);
+      //   const validPassword = await bcrypt.compare(password, user.password);
       if (password !== user.password) {
         return done(null, false, { message: "Incorrect password." });
       }
@@ -32,12 +30,12 @@ const passportConfig = (passport, users) => {
 
   passport.use(new LocalStrategy({ usernameField: "email" }, callback));
   passport.serializeUser((user, done) => done(null, user.id));
-  passport.deserializeUser((id, done) => {      
+  passport.deserializeUser((id, done) => {
     const user = findUserById(id, users);
     try {
-        return done(null, user);
+      return done(null, user);
     } catch (error) {
-        return done(err);
+      return done(err);
     }
   });
 };
